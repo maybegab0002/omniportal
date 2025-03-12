f05b694361f670382507bea265afffa4039be666
maybegab0002 <gabenciso119@gmail.com>
2025-03-12 10:27:26 +0800

    Update configuration for custom domain omniportals.ph

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/omniportal/',
+  base: 'https://omniportals.ph/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
