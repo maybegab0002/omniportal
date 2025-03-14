5082cca5866d8541017beadb1a7d21698c3dee59
maybegab0002 <gabenciso119@gmail.com>
2025-03-14 14:15:54 +0800

    Fix Vercel deployment with conditional base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/omniportal/',
+  base: process.env.VERCEL ? '/' : '/omniportal/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
