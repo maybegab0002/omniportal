6d8a90475c70e264e8a9508ef192ff225035ff67
maybegab0002 <gabenciso119@gmail.com>
2025-03-14 14:52:16 +0800

    Updates

--- config.js
+++
@@ -1,7 +0,0 @@
-// This file contains public configuration that will be available in the browser
-// Do NOT put sensitive information here
-window.OMNI_PORTAL_CONFIG = {
-  // These will be replaced with actual values during build or can be set manually
-  supabaseUrl: "https://krrumlttvcgcnislqoxc.supabase.co",
-  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtycnVtbHR0dmNnY25pc2xxb3hjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg2NzUxNjYsImV4cCI6MjA1NDI1MTE2Nn0.FlqXb5NLyKesUaNv3TnhdqGry1Dm3groxCwZ4cGsQLc"
-};
